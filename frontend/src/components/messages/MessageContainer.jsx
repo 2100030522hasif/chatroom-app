24fe29bdb005b2f10b6c119b87f1133d9188ffe7
Syed Hasif Alisha <2100030522@kluniversity.in>
2024-05-02 21:33:20 +0530

    Socket url updated

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import useConversation from "../../zustand/useConversation";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
+import "./styles.css";
 
 const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
@@ -15,7 +16,7 @@ const MessageContainer = () => {
 
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col'>
+		<div className='message-container md:min-w-[0px] flex flex-col'>
 			{!selectedConversation ? <NoChatSelected /> : (
         <>
 				{/* Header */}
@@ -35,7 +36,7 @@ export default MessageContainer;
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
 	return (
-		<div className='flex items-center justify-center w-full h-full'>
+		<div className='flex flex-col items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
 				<p>Welcome {authUser.fullName}!</p>
 				<p>Select a chat to start messaging</p>
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
+import "./styles.css";
 
 const Sidebar = () => {
 	return (
-		<div className='border-r border-[#45624e] p-4 flex flex-col'>
+		<div className='sidebar border-r border-[#45624e] p-4 flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
